b613308d430f3cab0ced36069c8c3b6e0be38100
willperez23 <william.perez.23@dartmouth.edu>
2022-08-03 23:53:48 -0400

    fixes

--- src/components/beepboop.js
+++ src/components/beepboop.js
@@ -22,7 +22,7 @@ const BeepBoop = (props) => {
           <h2>Case Study </h2>
           <div className="eighty">
             <h3>Purpose:</h3>
-            <p>As a language learning platform, BeepBoop
+            <p className="smaller">As a language learning platform, BeepBoop
               relies heavily on the teacher to student
               interaction. Beepboop is in the process
               of creating their MVP web/mobile platform
@@ -33,11 +33,11 @@ const BeepBoop = (props) => {
           </div>
           <div className="eighty">
             <h3>Timeline:</h3>
-            <p>10 weeks</p>
+            <p className="smaller">10 weeks</p>
           </div>
           <div className="eighty">
             <h3>Team:</h3>
-            <p>Salvatore Lupo - Designer <br />
+            <p className="smaller">Salvatore Lupo - Designer <br />
               William Perez - Designer <br />
               Melissa Valencia - Designer <br />
               Devon Saliga - Company CEO
@@ -46,11 +46,11 @@ const BeepBoop = (props) => {
           <div className="eighty">
             <h3>Tags:</h3>
             <div className="horizontalFlex verticalAlign additionalInfo">
-              <div className="beepBoopTag">Mobile</div>
-              <div className="beepBoopTag">Light UI</div>
-              <div className="beepBoopTag">Spanish</div>
-              <div className="beepBoopTag">Startup</div>
-              <div className="beepBoopTag">Education</div>
+              <div className="beepBoopTag smaller">Mobile</div>
+              <div className="beepBoopTag smaller">Light UI</div>
+              <div className="beepBoopTag smaller">Spanish</div>
+              <div className="beepBoopTag smaller">Startup</div>
+              <div className="beepBoopTag smaller">Education</div>
             </div>
           </div>
         </div>
@@ -59,7 +59,7 @@ const BeepBoop = (props) => {
       <div className="horizontalFlex spacebtwn btmSpacer">
         <div className="left-spacer">
           <h3 className="orangeText">The Challenge</h3>
-          <p>BeepBoop’s user interface relied on the Calendly
+          <p className="smaller">BeepBoop’s user interface relied on the Calendly
             and Zoom applications for the majority of the
             user flow. The design team was tasked with creating
             a mobile application for addressing the BeepBoop
@@ -69,7 +69,7 @@ const BeepBoop = (props) => {
         </div>
         <div className="right-spacer">
           <h3 className="orangeText">The Product</h3>
-          <p>Established in 2019, BeepBoop aims to help users
+          <p className="smaller">Established in 2019, BeepBoop aims to help users
             practice their Spanish pronunciacion through a series
             of increasingly challenging memory retention and
             pronunciation exercises. The proudct is influenced
@@ -80,7 +80,7 @@ const BeepBoop = (props) => {
       </div>
       <div className="left-spacer right btmSpacer">
         <h3 className="orangeText">Talking to the Instructors</h3>
-        <p>Kicking off our research stage, the design team conducted
+        <p className="smaller">Kicking off our research stage, the design team conducted
           5 user interviews with drill instructors and the CEO. We
           synthesized our findings into key pain points for our users.
           Additionaly, the team created mindmaps targeting core instructors
@@ -94,19 +94,19 @@ const BeepBoop = (props) => {
           <h3 className="orangeText">Pain Points</h3>
           <div className="horizontalFlex">
             <img className="pushRight pushDown" src={orangePencil} alt="orange pencil" />
-            <p className="bold">Handwriting personalized notes for post-drill feedback. </p>
+            <p className=" smaller bold">Handwriting personalized notes for post-drill feedback. </p>
           </div>
           <div className="horizontalFlex">
             <img className="pushRight pushDown" src={orangeTarget} alt="orange target" />
-            <p className="bold">Identifying VIP members & distributing correction time accordingly </p>
+            <p className="smaller bold">Identifying VIP members & distributing correction time accordingly </p>
           </div>
           <div className="horizontalFlex">
             <img className="pushRight pushDown" src={orangeCalendar} alt="orange calendar" />
-            <p className="bold">Scheduling drill instructing lessons an selecting course topics </p>
+            <p className=" smaller bold">Scheduling drill instructing lessons an selecting course topics </p>
           </div>
           <div className="horizontalFlex">
             <img className="pushRight pushDown" src={orangeChat} alt="orange chat" />
-            <p className="bold">Providing personalized post-drill feedback to each student  </p>
+            <p className=" smaller bold">Providing personalized post-drill feedback to each student  </p>
           </div>
         </div>
         <div className="right-spacer">
@@ -124,19 +124,19 @@ const BeepBoop = (props) => {
         </div>
         <div className="right-spacer">
           <h3 className="orangeText">Ideation - Crazy 8s</h3>
-          <p>The each member of the design team sketched eight screens in eight minutes, depicting the main features:</p>
+          <p className=" smaller">The each member of the design team sketched eight screens in eight minutes, depicting the main features:</p>
           <li>Profile page </li>
           <li>Live Drills - Video Streaming Page  </li>
           <li>Drill Course Catalog </li>
           <li>Course Dashboard </li>
-          <p>As a team we used the dot voting method to identify the screens that we wanted to prototype keeping in mind the points for virality for the proudct </p>
+          <p className=" smaller">As a team we used the dot voting method to identify the screens that we wanted to prototype keeping in mind the points for virality for the proudct </p>
         </div>
       </div>
 
       <div className="horizontalFlex verticalAlign btmSpacer">
         <div className="left-spacer">
           <h3 className="orangeText">Ideation - Crazy 8s</h3>
-          <p>In an attempt to deviate from industry standard navigation bar, the team decided to play with rounded corners and centered on the live streaming aspect.
+          <p className=" smaller">In an attempt to deviate from industry standard navigation bar, the team decided to play with rounded corners and centered on the live streaming aspect.
             We began with overlays as a way to decrease clicks need to reach other pages; however, after broader discussions we decided to set these pages as part of
             the navigation bar.
           </p>
@@ -152,11 +152,11 @@ const BeepBoop = (props) => {
       <div className="horizontalFlex verticalAlign btmSpacer">
         <div className="left-spacer">
           <h3 className="orangeText">Ideation - Crazy 8s</h3>
-          <p>Quick user testing on the greyscales confirmed the direction in whcih the team moved forward for the high fidelity screens. As a team we divided
+          <p className=" smaller">Quick user testing on the greyscales confirmed the direction in whcih the team moved forward for the high fidelity screens. As a team we divided
             the design work evenly, ensuring that we had the opportunity to collobarate on screens and ask for feedback throughout the process. After multiple
             scrapped screens we moved forward with testing versions of the the home, profile, and review screens:
           </p>
-          <p>User testing showed that the core funcitoinality of the app served its purposel; however gave multiple insights into new directions and features
+          <p className=" smaller">User testing showed that the core funcitoinality of the app served its purposel; however gave multiple insights into new directions and features
             that could be added later down the road.
           </p>
         </div>
@@ -167,13 +167,13 @@ const BeepBoop = (props) => {
 
       <div className="orange btmSpacer">
         <h2 className="bold left-spacer">“Everything from scheduling, prep, teaching, and feedback is taken care of”</h2>
-        <p className="left-spacer"> - Drill Instructor</p>
+        <p className="left-spacer smaller"> - Drill Instructor</p>
       </div>
 
       <div className="horizontalFlex btmSpacer">
         <div className="left-spacer">
           <h3 className="orangeText">Brand Identity</h3>
-          <p>Being in such early stages of the start up, there was so much flexibility to take the company
+          <p className=" smaller">Being in such early stages of the start up, there was so much flexibility to take the company
             in any given direction. However, the design team focused on the user research and expereince portion
             of the sprint before entering in conversations about the brand and the interface components. The marketing
             team developed a brandbook that was then used to create the styling library for the sprint.
@@ -190,7 +190,7 @@ const BeepBoop = (props) => {
         </div>
         <div className="right">
           <h3 className="orangeText">Reflecting</h3>
-          <p>Iterating on design is a necessary part of the process.
+          <p className="smaller">Iterating on design is a necessary part of the process.
             Leaving this design sprint with BeepBoop, I am congizant
             that not every idea will be best for the user. The answers
             lie with the iusers. From user test, we quickly stear our
