9aa473bb43738d9134d3743e3df06e752af73118
willperez23 <william.perez.23@dartmouth.edu>
2022-08-06 23:31:38 -0400

    error

---
+++ src/components/error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = (props) => {
+  return (
+    <div id="portfolioFlex">
+      <h1>oops 404 error!</h1>
+      <p>This page is underconstruction! Check back soon for updates. I&apos;m constantly updating this site :))</p>
+    </div>
+
+  );
+};
+
+export default Error;
