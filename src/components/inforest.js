269a2c3fe8e74cb075ddc94fb8e383fd5add00f8
willperez23 <william.perez.23@dartmouth.edu>
2023-07-05 23:05:08 -0700

    fix?

--- src/components/inforest.js
+++ src/components/inforest.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import '../styling/inforest.scss';
-import ProjectTitle from './ProjectTitle';
+import ProjectTitle from './projectTitle';
 
 const inforest = (props) => {
   useEffect(() => {
